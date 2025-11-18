0b10194335620f39f51d0d5adba904d68cd66f8d
lwmacct <lwmacct@icloud.com>
2025-11-18 15:37:06 +0800

    -

--- web/src/App.vue
+++ web/src/App.vue
@@ -1,7 +1,7 @@
 <script setup lang="ts"></script>
 
 <template>
-  <h1>You did it!888888</h1>
+  <h1>You did it!</h1>
   <p>
     Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
     documentation
